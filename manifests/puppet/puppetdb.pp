377ccca098699e0ea447cc446126c311f4895f8d
Evgeniy Evtushenko <evgeniye@crytek.com>
2014-11-21 07:46:28 +0200

    PuppetDB profile improvements. PostgreSQL installation separated from PuppetDB (now it uses puppetlabs-postgresql module instead of internal classes from puppetdb module).

--- manifests/puppet/puppetdb.pp
+++ manifests/puppet/puppetdb.pp
@@ -15,7 +15,7 @@
 #
 # === Links
 #
-# * {puppetlabs/puppetdb}[https://forge.puppetlabs.com/puppetlabs/puppetdb/dependencies]
+# * {puppetlabs/puppetdb}[https://forge.puppetlabs.com/puppetlabs/puppetdb]
 #
 # === Authors
 #
@@ -23,10 +23,36 @@
 #
 class profile::puppet::puppetdb {
   ## Hiera lookups
-  $listen_address = hiera('profile::puppet::puppetdb::listen_address')
+  $listen_address	= hiera('profile::puppet::puppetdb::listen_address')
+  $ssl_listen_address	= hiera('profile::puppet::puppetdb::ssl_listen_address')
+  $database_host    	= hiera('profile::puppet::puppetdb::database_host')
+  $database_name	= hiera('profile::puppet::puppetdb::database_name')
+  $database_user    	= hiera('profile::puppet::puppetdb::database_user')
+  $database_password    = hiera('profile::puppet::puppetdb::database_password')
 
-  include ::puppetdb::master::config
-  class { '::puppetdb':
-    listen_address => $listen_address,
+  $puppetdb_server  	= $::fqdn
+
+  # Configure PostgreSQL server
+  include profile::postgresql::globals
+  include profile::postgresql::server
+  ::postgresql::server::db { 'PuppetDB_database':
+    dbname    => $database_name,
+    user      => $database_user,
+    password  => postgresql_password($database_user, $database_password),
+  }
+
+  # Configure PuppetDB server
+  class { '::puppetdb::server':
+    database_host	=> $database_host,
+    listen_address	=> $listen_address,
+    ssl_listen_address	=> $ssl_listen_address,
+    database_name	=> $database_name,
+    database_username 	=> $database_user,
+    database_password 	=> $database_password,
+  }
+
+  # Configure puppet master
+  class { '::puppetdb::master::config':
+    puppetdb_server => $puppetdb_server,
   }
 }
